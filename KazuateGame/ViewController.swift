c0e3d1c3ae820b8bc870f1fccce704364976adc4
nin-f <fukayaninsei@gmail.com>
2022-08-21 12:28:30 +0900

    Initial Commit

---
+++ KazuateGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KazuateGame
+//
+//  Created by NinseiFukaya on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var start: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+   
+    
+}
---
+++ KazuateGame/kazuateViewController.swift
@@ -0,0 +1,47 @@
+//
+//  kazuateViewController.swift
+//  KazuateGame
+//
+//  Created by NinseiFukaya on 2022/08/21.
+//
+
+import UIKit
+
+class kazuateViewController: UIViewController {
+
+    @IBOutlet var nyuryoku1: UILabel!
+    @IBOutlet var nyuryoku2: UILabel!
+    @IBOutlet var nyuryoku3: UILabel!
+    
+    @IBOutlet var n0: UIButton!
+    @IBOutlet var n1: UIButton!
+    @IBOutlet var n2: UIButton!
+    @IBOutlet var n3: UIButton!
+    @IBOutlet var n4: UIButton!
+    @IBOutlet var n5: UIButton!
+    @IBOutlet var n6: UIButton!
+    @IBOutlet var n7: UIButton!
+    @IBOutlet var n8: UIButton!
+    @IBOutlet var n9: UIButton!
+    
+    @IBOutlet var EAT: UILabel!
+    @IBOutlet var BITE: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
